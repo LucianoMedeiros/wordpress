eba648bbde244c921661cac1af716adf5e42c621
Luciano Medeiros <luciano.medeiros@aberto.com.br>
2020-10-04 12:03:20 -0300

    estrutura mínima para rodar o tema

---
+++ wp-content/themes/newberry/index.php
@@ -0,0 +1,13 @@
+<?= get_header(); ?>
+
+<div>
+    <main>
+        <section>
+            <h1><?= the_title(); ?></h1>
+        </section>
+    </main>
+    
+    <?= get_sidebar(); ?>
+</div>
+
+<?= get_footer(); ?>
